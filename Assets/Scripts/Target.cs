6ef0344e957e9ca041ff6e660f5c303af72ca558
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2025-02-05 12:39:21 -0500

    5.2 complete

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -8,23 +8,33 @@ public class Target : MonoBehaviour
     float minSpeed = 12;
     float maxSpeed = 20;
     float maxTorque = 10;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(Vector2.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-maxTorque,maxTorque), Random.Range(-maxTorque,maxTorque), Random.Range(-maxTorque,maxTorque), ForceMode.Impulse);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.y < -2)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
